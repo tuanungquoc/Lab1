0e236967ff8efe86b07044d8474954e616598520
Tuan Ung <tuanungquoc@gmail.com>
2017-10-02 19:30:32 -0700

    fix condition flow

--- app/src/main/java/com/example/t0u000c/lab1/FirstOperandState.java
+++ app/src/main/java/com/example/t0u000c/lab1/FirstOperandState.java
@@ -15,26 +15,27 @@ public class FirstOperandState implements ICalculatorState {
     public void pressNumber(String input) {
 
         if(calculatorControl.getOperator().equals("=") && input.equals("0")){
-            return;
+            //return;
         }else if(calculatorControl.getOperator().equals("=") && !input.equals("0")){
             calculatorControl.setOperator("");
             calculatorControl.setSign("");
             calculatorControl.setTexResultView(input);
-            return;
-        }
-        String currentTextView = calculatorControl.getTextResultView();
-        String sign = calculatorControl.getSign();
+            //return;
+        }else {
+            String currentTextView = calculatorControl.getTextResultView();
+            String sign = calculatorControl.getSign();
 
-        if((currentTextView.length() <= 6 && !sign.equals("-")) || (currentTextView.length() <= 7 && sign.equals("-")) ) {
+            if ((currentTextView.length() <= 6 && !sign.equals("-")) || (currentTextView.length() <= 7 && sign.equals("-"))) {
 
-            int resultInt = Integer.parseInt((sign.equals("-") ? currentTextView.substring(0, currentTextView.length() - 1) : currentTextView) + "" + input);
-            String temp = resultInt + "" + (sign.equals("-") ? "-" : "");
-            calculatorControl.setTexResultView(temp);
-        }else{
-            calculatorControl.setTexResultView("!ERROR");
-            calculatorControl.setSign("");
-            calculatorControl.setFirstOperand(0);
-            calculatorControl.setState(calculatorControl.getStartState());
+                int resultInt = Integer.parseInt((sign.equals("-") ? currentTextView.substring(0, currentTextView.length() - 1) : currentTextView) + "" + input);
+                String temp = resultInt + "" + (sign.equals("-") ? "-" : "");
+                calculatorControl.setTexResultView(temp);
+            } else {
+                calculatorControl.setTexResultView("!ERROR");
+                calculatorControl.setSign("");
+                calculatorControl.setFirstOperand(0);
+                calculatorControl.setState(calculatorControl.getStartState());
+            }
         }
     }
 
@@ -46,26 +47,27 @@ public class FirstOperandState implements ICalculatorState {
                 calculatorControl.setSign("");
                 calculatorControl.setFirstOperand(0);
                 calculatorControl.setState(calculatorControl.getStartState());
-                return;
-            }
-            String temp = calculatorControl.getTextResultView();
-            String sign = temp.substring( temp.length()-1).equals("-")? "-":"";
+                //return;
+            }else {
+                String temp = calculatorControl.getTextResultView();
+                String sign = temp.substring(temp.length() - 1).equals("-") ? "-" : "";
 
-            int firstOperand = sign.equals("-") ? -1*Integer.parseInt((temp+"").substring(0, temp.length()-1))
-                : Integer.parseInt(temp+"");
-            if(!input.equals("=")) {
-                calculatorControl.setFirstOperand(firstOperand);
-                calculatorControl.setState(calculatorControl.getOperatorState());
-                calculatorControl.setOperator(input);
-            }else{
-                if(calculatorControl.getSign().equals("*") || calculatorControl.getSign().equals("/")){
-                    calculatorControl.setTexResultView("!ERROR");
-                    calculatorControl.setFirstOperand(0);
-                    calculatorControl.setSign("");
-                    calculatorControl.setState(calculatorControl.getStartState());
-                    return;
+                int firstOperand = sign.equals("-") ? -1 * Integer.parseInt((temp + "").substring(0, temp.length() - 1))
+                        : Integer.parseInt(temp + "");
+                if (!input.equals("=")) {
+                    calculatorControl.setFirstOperand(firstOperand);
+                    calculatorControl.setState(calculatorControl.getOperatorState());
+                    calculatorControl.setOperator(input);
+                } else {
+                    if (calculatorControl.getSign().equals("*") || calculatorControl.getSign().equals("/")) {
+                        calculatorControl.setTexResultView("!ERROR");
+                        calculatorControl.setFirstOperand(0);
+                        calculatorControl.setSign("");
+                        calculatorControl.setState(calculatorControl.getStartState());
+                        return;
+                    }
+                    calculatorControl.setOperator(input);
                 }
-                calculatorControl.setOperator(input);
             }
     }
 
